69a1b9d8524a94ac937ca9aaf05a1e058021a59a
Samuel Blattner <samworks@gmx.net>
2020-01-17 12:51:23 +0100

    Add new envs and evals

--- batch_eval_0011_0014.sh
+++ batch_eval_0011_0014.sh
@@ -1,22 +1,5 @@
 #/bin/bash
 
-python main.py --purpose train --env 0011-all-cases --gpu_no 0 --net_type retina-resnet152 --data_dir=/home/blsa/projects/confidential/wound-detection/data/ --checkpoint_dir=/home/blsa/projects/confidential/wound-detection/checkpoints/ > 0011-retina152-train.log 2>&1
-python main.py --purpose evaluate --env 0011-all-cases --gpu_no 0 --net_type retina-resnet152 --data_dir=/home/blsa/projects/confidential/wound-detection/data/ --checkpoint_dir=/home/blsa/projects/confidential/wound-detection/checkpoints/ --eval_dir=/home/blsa/projects/confidential/wound-detection/evaluation/
-python main.py --purpose evaluate --env 0011-all-cases --gpu_no 0 --full_size_eval true --net_type retina-resnet152 --data_dir=/home/blsa/projects/confidential/wound-detection/data/ --checkpoint_dir=/home/blsa/projects/confidential/wound-detection/checkpoints/ --eval_dir=/home/blsa/projects/confidential/wound-detection/evaluation/
-
-python main.py --purpose train --env 0011b-all-cases --gpu_no 0 --net_type retina-resnet152 --data_dir=/home/blsa/projects/confidential/wound-detection/data/ --checkpoint_dir=/home/blsa/projects/confidential/wound-detection/checkpoints/ > 0011b-retina152-train.log 2>&1
-python main.py --purpose evaluate --env 0011b-all-cases --gpu_no 0 --net_type retina-resnet152 --data_dir=/home/blsa/projects/confidential/wound-detection/data/ --checkpoint_dir=/home/blsa/projects/confidential/wound-detection/checkpoints/ --eval_dir=/home/blsa/projects/confidential/wound-detection/evaluation/
-python main.py --purpose evaluate --env 0011b-all-cases --gpu_no 0 --full_size_eval true --net_type retina-resnet152 --data_dir=/home/blsa/projects/confidential/wound-detection/data/ --checkpoint_dir=/home/blsa/projects/confidential/wound-detection/checkpoints/ --eval_dir=/home/blsa/projects/confidential/wound-detection/evaluation/
-
-python main.py --purpose train --env 0011c-all-cases --gpu_no 0 --net_type retina-resnet152 --data_dir=/home/blsa/projects/confidential/wound-detection/data/ --checkpoint_dir=/home/blsa/projects/confidential/wound-detection/checkpoints/ > 0011c-retina152-train.log 2>&1
-python main.py --purpose evaluate --env 0011c-all-cases --gpu_no 0 --net_type retina-resnet152 --data_dir=/home/blsa/projects/confidential/wound-detection/data/ --checkpoint_dir=/home/blsa/projects/confidential/wound-detection/checkpoints/ --eval_dir=/home/blsa/projects/confidential/wound-detection/evaluation/
-python main.py --purpose evaluate --env 0011c-all-cases --gpu_no 0 --full_size_eval true --net_type retina-resnet152 --data_dir=/home/blsa/projects/confidential/wound-detection/data/ --checkpoint_dir=/home/blsa/projects/confidential/wound-detection/checkpoints/ --eval_dir=/home/blsa/projects/confidential/wound-detection/evaluation/
-
-python main.py --purpose train --env 0011d-all-cases --gpu_no 0 --net_type retina-resnet152 --data_dir=/home/blsa/projects/confidential/wound-detection/data/ --checkpoint_dir=/home/blsa/projects/confidential/wound-detection/checkpoints/ > 0011d-retina152-train.log 2>&1
-python main.py --purpose evaluate --env 0011d-all-cases --gpu_no 0 --net_type retina-resnet152 --data_dir=/home/blsa/projects/confidential/wound-detection/data/ --checkpoint_dir=/home/blsa/projects/confidential/wound-detection/checkpoints/ --eval_dir=/home/blsa/projects/confidential/wound-detection/evaluation/
-python main.py --purpose evaluate --env 0011d-all-cases --gpu_no 0 --full_size_eval true --net_type retina-resnet152 --data_dir=/home/blsa/projects/confidential/wound-detection/data/ --checkpoint_dir=/home/blsa/projects/confidential/wound-detection/checkpoints/ --eval_dir=/home/blsa/projects/confidential/wound-detection/evaluation/
-
-
 python main.py --purpose evaluate --env 0011b-all-cases-1024-body-shots-eval --gpu_no 3 --net_type retina-resnet152 --data_dir=/home/blsa/projects/confidential/wound-detection/data/ --checkpoint_dir=/home/blsa/projects/confidential/wound-detection/checkpoints/ --eval_dir=/home/blsa/projects/confidential/wound-detection/evaluation/
 python main.py --purpose evaluate --env 0011b-all-cases-1024-body-shots-eval --gpu_no 3 --full_size_eval true --net_type retina-resnet152 --data_dir=/home/blsa/projects/confidential/wound-detection/data/ --checkpoint_dir=/home/blsa/projects/confidential/wound-detection/checkpoints/ --eval_dir=/home/blsa/projects/confidential/wound-detection/evaluation/
 

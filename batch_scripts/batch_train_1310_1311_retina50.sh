46c646cf7007ea382d76adf40e47d8ce37dbb577
Samuel Blattner <samworks@gmx.net>
2020-02-17 01:41:09 +0100

    Add envs

---
+++ batch_scripts/batch_train_1310_1311_retina50.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+python main.py --purpose train    --env 1310-joint-cases-extracted-close-up-wounds  --loss_patience=3 --val_loss_patience=3                                     --gpu_no 0 --net_type retina-resnet50 --data_dir=/home/samuelblattner/projects/ffhs-bachelor-thesis-wound-detection/data/vanilla_datasets --checkpoint_dir=/checkpoints/ --eval_dir=/checkpoints/evaluation > train-1310-retina50.log 2>&1
+python main.py --purpose evaluate --env 1310-joint-cases-extracted-close-up-wounds                                       --gpu_no 0 --net_type retina-resnet50 --data_dir=/home/samuelblattner/projects/ffhs-bachelor-thesis-wound-detection/data/vanilla_datasets --checkpoint_dir=/checkpoints/ --eval_dir=/checkpoints/evaluation                                     --eval_images --eval_heatmaps
+python main.py --purpose evaluate --env 1310-joint-cases-extracted-close-up-wounds-body-shots-eval                       --gpu_no 0 --net_type retina-resnet50 --data_dir=/home/samuelblattner/projects/ffhs-bachelor-thesis-wound-detection/data/vanilla_datasets --checkpoint_dir=/checkpoints/ --eval_dir=/checkpoints/evaluation --eval_name_suffix=-body-shots-eval --eval_images --eval_heatmaps
+python main.py --purpose evaluate --env 1310-joint-cases-extracted-close-up-wounds-cases-eval                            --gpu_no 0 --net_type retina-resnet50 --data_dir=/home/samuelblattner/projects/ffhs-bachelor-thesis-wound-detection/data/vanilla_datasets --checkpoint_dir=/checkpoints/ --eval_dir=/checkpoints/evaluation --eval_name_suffix=-cases-eval      --eval_images --eval_heatmaps
+
+mkdir -p /checkpoints/1311a_Joint_Cases_Extracted_Closeup_Wounds_Fine_Tuning--RetinaNet-Resnet50
+cp /checkpoints/1310a_Joint_Cases_Extracted_Closeup_Wounds--RetinaNet-Resnet50/*latest.h5 /checkpoints/1311a_Joint_Cases_Extracted_Closeup_Wounds_Fine_Tuning--RetinaNet-Resnet50
+mv /checkpoints/1311a_Joint_Cases_Extracted_Closeup_Wounds_Fine_Tuning--RetinaNet-Resnet50/*latest.h5 /checkpoints/1311a_Joint_Cases_Extracted_Closeup_Wounds_Fine_Tuning--RetinaNet-Resnet50/1311a_Joint_Cases_Extracted_Closeup_Wounds_Fine_Tuning--RetinaNet-Resnet50_0001.h5
+
+mkdir -p /checkpoints/1311b_Joint_Cases_Extracted_Closeup_Wounds_Fine_Tuning--RetinaNet-Resnet50
+cp /checkpoints/1310_Joint_Cases_Extracted_Closeup_Wounds--RetinaNet-Resnet50/*latest.h5 /checkpoints/1311b_Joint_Cases_Extracted_Closeup_Wounds_Fine_Tuning--RetinaNet-Resnet50
+mv /checkpoints/1311b_Joint_Cases_Extracted_Closeup_Wounds_Fine_Tuning--RetinaNet-Resnet50/*latest.h5 /checkpoints/1311b_Joint_Cases_Extracted_Closeup_Wounds_Fine_Tuning--RetinaNet-Resnet50/1311b_Joint_Cases_Extracted_Closeup_Wounds_Fine_Tuning--RetinaNet-Resnet50_0001.h5
+
+mkdir -p /checkpoints/1311c_Joint_Cases_Extracted_Closeup_Wounds_Fine_Tuning--RetinaNet-Resnet50
+cp /checkpoints/1310c_Joint_Cases_Extracted_Closeup_Wounds--RetinaNet-Resnet50/*latest.h5 /checkpoints/1311c_Joint_Cases_Extracted_Closeup_Wounds_Fine_Tuning--RetinaNet-Resnet50
+mv /checkpoints/1311c_Joint_Cases_Extracted_Closeup_Wounds_Fine_Tuning--RetinaNet-Resnet50/*latest.h5 /checkpoints/1311c_Joint_Cases_Extracted_Closeup_Wounds_Fine_Tuning--RetinaNet-Resnet50/1311c_Joint_Cases_Extracted_Closeup_Wounds_Fine_Tuning--RetinaNet-Resnet50_0001.h5
+
+mkdir -p /checkpoints/1311d_Joint_Cases_Extracted_Closeup_Wounds_Fine_Tuning--RetinaNet-Resnet50
+cp /checkpoints/1310d_Joint_Cases_Extracted_Closeup_Wounds--RetinaNet-Resnet50/*latest.h5 /checkpoints/1311d_Joint_Cases_Extracted_Closeup_Wounds_Fine_Tuning--RetinaNet-Resnet50
+mv /checkpoints/1311d_Joint_Cases_Extracted_Closeup_Wounds_Fine_Tuning--RetinaNet-Resnet50/*latest.h5 /checkpoints/1311d_Joint_Cases_Extracted_Closeup_Wounds_Fine_Tuning--RetinaNet-Resnet50/1311d_Joint_Cases_Extracted_Closeup_Wounds_Fine_Tuning--RetinaNet-Resnet50_0001.h5
+
+python main.py --purpose train    --env 1311-joint-cases-extracted-close-up-wounds-fine-tuning     --loss_patience=3 --val_loss_patience=3                      --gpu_no=0 --net_type retina-resnet50 --data_dir=/home/samuelblattner/projects/ffhs-bachelor-thesis-wound-detection/data/vanilla_datasets --checkpoint_dir=/checkpoints --eval_dir=/checkpoints/evaluation > train-1311-retina50.log 2>&1
+python main.py --purpose evaluate --env 1311-joint-cases-extracted-close-up-wounds-fine-tuning                           --gpu_no=0 --net_type retina-resnet50 --data_dir=/home/samuelblattner/projects/ffhs-bachelor-thesis-wound-detection/data/vanilla_datasets --checkpoint_dir=/checkpoints --eval_dir=/checkpoints/evaluation                                     --eval_images --eval_heatmaps
+python main.py --purpose evaluate --env 1311-joint-cases-extracted-close-up-wounds-fine-tuning-body-shots-eval           --gpu_no=0 --net_type retina-resnet50 --data_dir=/home/samuelblattner/projects/ffhs-bachelor-thesis-wound-detection/data/vanilla_datasets --checkpoint_dir=/checkpoints --eval_dir=/checkpoints/evaluation                                     --eval_images --eval_heatmaps
+python main.py --purpose evaluate --env 1311-joint-cases-extracted-close-up-wounds-fine-tuning-cases-eval                --gpu_no=0 --net_type retina-resnet50 --data_dir=/home/samuelblattner/projects/ffhs-bachelor-thesis-wound-detection/data/vanilla_datasets --checkpoint_dir=/checkpoints --eval_dir=/checkpoints/evaluation                                     --eval_images --eval_heatmaps
